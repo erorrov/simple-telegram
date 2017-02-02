5fd43f26cb69e7985d5c184f33811ce25287c987
Egor Egorov <erorrov@gmail.com>
2017-02-03 00:19:55 +0300

    custom keyboards

--- telegram.php
+++ telegram.php
@@ -1,11 +1,10 @@
 <?php
-namespace tg;
+//namespace tg;
 
 class Telegram{
 
   private $token;
   private $data = array();
-  private $ch;
 
   function __construct($token){
     $this->token = $token;
@@ -16,14 +15,40 @@ class Telegram{
     return json_decode(file_get_contents("php://input"), true);
   }
 
-  public function sendRequest($method, array $content){
+  public function loadFile($path){
+    return curl_file_create($path);
+  }
+
+  //¯\_(ツ)_/¯
+  public function buildInlineButton(array $params){
+    return $params;
+  }
+
+  //¯\_(ツ)_/¯
+  public function buildReplyButton(array $params){
+    return $params;
+  }
+
+  public function buildInlineKeyboard(array $buttons){
+    return json_encode(["inline_keyboard" => $buttons], true);
+  }
+
+  public function buildReplyKeyboard(array $buttons, array $params = array()){
+    return json_encode(["keyboard" => $buttons, $params], true);
+  }
+
+  public function removeReplyKeyboard(array $params = array()){
+    return json_encode(["remove_keyboard" => true, $params], true);
+  }
+
+  public function sendRequest($method, array $params){
     $ch = curl_init();
     $url = "https://api.telegram.org/bot".$this->token."/".$method;
     curl_setopt($ch, CURLOPT_URL, $url);
     curl_setopt($ch, CURLOPT_HEADER, false);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
     curl_setopt($ch, CURLOPT_POST, 1);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, $content);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $params);
     curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
     $result = curl_exec($ch);
     return json_decode($result, true);
