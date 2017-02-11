a5b10f8cfa4a0c171b472d2efae59dd87cbdcfac
Egor Egorov <erorrov@gmail.com>
2017-02-11 11:05:34 +0300

    i wanna sleep

--- examples/WebHook.php
+++ examples/WebHook.php
@@ -1,38 +1,53 @@
 <?php
+/**
+ * Telegram webhook bot Example.
+ * @author Egor Egorov <github@erorrov.ru>
+ */
+
+//Load class
 require("telegram.php");
 
+//Instances class
 $bot_token = "BOT_TOKEN";
 $tg = new Telegram($bot_token);
 
+//Get an array of current message
 $result = $tg->getData();
 $text = $result['message']['text'];
 $chatID = $result['message']['chat']['id'];
 
 if($text == "/start"){
+  //Build buttons
   $btn1 = $tg->buildReplyButton(["text" => "Ping"]);
   $btn2 = $tg->buildReplyButton(["text" => "🌅"]);
   $btn3 = $tg->buildReplyButton(["text" => "Close keyboard"]);
   $btn4 = $tg->buildReplyButton(["text" => "Help"]);
 
+  //Position of buttons on keyboard
   $buttons = [
     [$btn1, $btn2],
     [$btn4],
     [$btn3]
   ];
 
+  //Build keyboard
   $rm = $tg->buildReplyKeyboard($buttons);
 
+  //Send a message
   $params = ["chat_id" => $chatID, "text" => "Hey. What do you want?", "reply_markup" => $rm];
   $tg->sendRequest("sendMessage", $params);
 }
 
 if($text == "Ping"){
+  //Send simple text messages
   $params = ["chat_id" => $chatID, "text" => "Pong"];
   $tg->sendRequest("sendMessage", $params);
 }
 
 if($text == "🌅"){
+  //Load gile at $img
   $img = $tg->loadFile("test.png");
+  //Sending a message with $img as photo
   $params = ["chat_id" => $chatID, "photo" => $img, "caption" => "This is a simple example how to send a picture"];
   $tg->sendRequest("sendPhoto", $params);
 }
@@ -44,6 +59,7 @@ if($text == "Close keyboard"){
 }
 
 if($text == "Help"){
+  //By analogy with that in line 20
   $btn1 = $tg->buildInlineButton(["text" => "📄 View on GitHub", "url" => "https://github.com/erorrov/simple-telegram"]);
   $btn2 = $tg->buildInlineButton(["text" => "Author", "url" => "https://t.me/erorrov"]);
 
